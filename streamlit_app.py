0dd0ada82bb394cd12bd85b2cb67f3e161e97615
rkzzone <muhamad.erik@ui.ac.id>
2024-11-14 17:05:34 +0700

    fifth commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,5 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Projects Data Structures"
-)
-
 queue_page = st.Page(
     page="Projects/Queue.py",
     title="Queue",
@@ -13,13 +9,11 @@ queue_page = st.Page(
 homepage_page = st.Page(
     page="Projects/Homepage.py",
     title="Homepage",
-    default=True,
 )
 
 projects_page = st.Page(
     page="Projects/Project.py",
     title="Project",
-    default=True,
 )
 
 pg = st.navigation(pages=[queue_page, homepage_page, projects_page])
